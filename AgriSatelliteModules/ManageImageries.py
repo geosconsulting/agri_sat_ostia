06eb0b9993aa20bb6a1507ffd1754aa9bd101a7c
fabio <fabiolana.utenze@gmail.com>
2022-04-04 17:05:03 +0200

    Almost Final - Already creates Cube with XARRAY but must use EPSG:4326 to ease management

--- AgriSatelliteModules/ManageImageries.py
+++ AgriSatelliteModules/ManageImageries.py
@@ -4,12 +4,12 @@ import shutil
 from datetime import datetime
 from typing import List
 from zipfile import ZipFile
-import matplotlib.pyplot as plt
 import geopandas as gpd
 import rasterio
 from rasterio.mask import mask
+from rasterio.plot import show
 from pathlib import Path
-import xarray as xr
+import matplotlib.pyplot as plt
 
 # Custom Modules
 from common_logger import logger
@@ -21,18 +21,39 @@ def get_coordinates(gdf_curr):
     return [json.loads(gdf_curr.to_json())['features'][0]['geometry']]
 
 
-def clip_images_in_dir(root_name_dir: str, clip_geojson: str) -> None:
+def fetch_images(root_name_dir, resolution=20, extension='.jp2'):
+    # Read images
+    images = []
+
+    for f in os.listdir(root_name_dir):
+        if f.endswith(extension):
+            find_res = re.search(str(resolution) + 'm', f)
+            if find_res:
+                images.append(f)
+    return images
+
+
+def clip_images_in_dir(root_name_dir: str, resolution: str, clip_geojson: str) -> None:
+    """Function to clip images in a directory
+
+
+        Arguments:  root_name_dir {str} -- Directory where images are stored
+                    resolution {str} -- Resolution of images to be clipped
+                    clip_geojson {str} -- Path to geojson file containing clip geometry
+
+        Returns:   None
+    """
 
     new_dir = f"clipped_images/{root_name_dir}"
     Path(new_dir).mkdir(parents=True, exist_ok=True)
 
     logger.info('Clipping images in {}'.format(root_name_dir))
 
-    # Read images
-    images = []
-    for f in os.listdir(root_name_dir):
-        if f.endswith('.jp2'):
-            images.append(f)
+    images = fetch_images(root_name_dir, resolution=resolution, extension='.jp2')
+    # images = []
+    # for f in os.listdir(root_name_dir):
+    #     if f.endswith('.jp2'):
+    #         images.append(f)
 
     # Read and reproject geojson
     clip_shape = gpd.read_file(clip_geojson)
@@ -45,12 +66,16 @@ def clip_images_in_dir(root_name_dir: str, clip_geojson: str) -> None:
         with rasterio.open(os.path.join(root_name_dir, image)) as src:
             # Read image
             image_array = src.read(1)
+
             # Get image metadata
             image_meta = src.meta.copy()
+
             # Get image crs
             image_crs = src.crs
+
             # Get image transform
             image_transform = src.transform
+
             # Get image bounds
             image_bounds = src.bounds
 
@@ -60,23 +85,42 @@ def clip_images_in_dir(root_name_dir: str, clip_geojson: str) -> None:
             # Clip image
             clipped_image, clipped_image_transform = mask(src, coords, crop=True)
 
-            # fig, ax = plt.subplots(figsize=(5, 6))
-            # show(clipped_image, ax=ax, cmap="Blues")
-            # plt.show()
-
             image_meta.update({"driver": "GTiff",
                                "height": clipped_image.shape[1],
                                "width": clipped_image.shape[2],
                                "transform": clipped_image_transform})
 
             # Create new image
-            with rasterio.open(os.path.join(new_dir, image), 'w', **image_meta) as dst:
+            image_changed_extension = re.sub(r'\.jp2', '.tif', image)
+            with rasterio.open(os.path.join(new_dir, image_changed_extension), 'w', **image_meta) as dst:
                 # save the resulting raster
                 dst.write(clipped_image)
 
 
+def plot_images(dir_name: str, resolution: int, poly_cut: str) -> None:
+    if resolution == 10:
+        row_tiles, col_tiles = 2, 2
+    elif resolution == 20:
+        row_tiles, col_tiles = 2, 4
+
+    fig, axs = plt.subplots(row_tiles, col_tiles)
+
+    images = fetch_images(dir_name, resolution=resolution, extension='.tif')
+
+    for img in images:
+        ax = axs.flat[images.index(img)]
+        img_path = os.path.join(dir_name, img)
+        with rasterio.open(img_path) as src:
+            show(src.read(1), ax=ax, cmap="gray")
+            ax.set_title(img[7:14])
+            ax.axis('off')
+            plt.tight_layout()
+
+    plt.show()
+
+
 class ManageSatelliteImages:
-    """ Class to unzip extract and clip Sentinel2 satellite images """
+    """ Class to unzip, extract and clip Sentinel2 satellite images """
 
     def __init__(self, zipdir='zipfiles'):
         self.zipdir = zipdir
@@ -97,29 +141,34 @@ class ManageSatelliteImages:
 
     def extract_images_in_zip(self, single_zip_file: str, resolution: list[int] = 20) -> str:
 
-        current_name_dir = single_zip_file.split('.')[0][-22:-7]
-        os.mkdir(current_name_dir)
-        logger.info('Managing images for {}'.format(current_name_dir))
+        # current_name_dir = single_zip_file.split('.')[0][-22:-7]
+        current_name_dir = single_zip_file.split('.')[0].split("_")[-1][0:8]
 
-        with ZipFile(os.path.join(self.zipdir, single_zip_file)) as zip_file:
-            # Iterate over the file names
-            for member in zip_file.namelist():
-                filename = os.path.basename(member)
-                if not filename:
-                    continue
+        if not os.path.exists(current_name_dir):
+            logger.info('{} does not exist creating...'.format(current_name_dir))
+            os.mkdir(current_name_dir)
+            logger.info('Managing images for {}'.format(current_name_dir))
+            with ZipFile(os.path.join(self.zipdir, single_zip_file)) as zip_file:
+                # Iterate over the file names
+                for member in zip_file.namelist():
+                    filename = os.path.basename(member)
+                    if not filename:
+                        continue
 
-                if f'IMG_DATA/R{resolution}m/T' in member:
-                    if member.endswith('.jp2'):
-                        search_string = member.split('/')[-1].split('.')[0][-7:]
-                        x = re.findall(f"B?[0-9]_{resolution}m$", search_string)
-                        if x:
-                            # Extract a single file from zip
-                            # zip_file.extract(member, root_name_dir)
-                            source = zip_file.open(member)
-                            target = open(os.path.join(current_name_dir, filename), "wb")
-                            with source, target:
-                                shutil.copyfileobj(source, target)
-                            logger.info('Extracting {}'.format(filename))
+                    if f'IMG_DATA/R{resolution}m/T' in member:
+                        if member.endswith('.jp2'):
+                            search_string = member.split('/')[-1].split('.')[0][-7:]
+                            real_band = re.findall(f"B?[0-9]_{resolution}m$", search_string)
+                            if real_band:
+                                # Extract a single file from zip
+                                # zip_file.extract(member, root_name_dir)
+                                source = zip_file.open(member)
+                                target = open(os.path.join(current_name_dir, filename), "wb")
+                                with source, target:
+                                    shutil.copyfileobj(source, target)
+                                logger.info('Extracting {}'.format(filename))
+        else:
+            logger.info('{} already exists'.format(current_name_dir))
 
         return current_name_dir
 
@@ -148,4 +197,3 @@ class ManageSatelliteImages:
         # time_var = xr.Variable('time', geotiff_list,string_slice=(12, -4)))
 
         return geotiff_list, date_date_unique, date_bands_unique
-
--- Main.py
+++ Main.py
@@ -9,17 +9,24 @@ def main():
     image_manager_obj = ManageImageries.ManageSatelliteImages()
     logger.info(image_manager_obj.zipped_files)
 
-    # list_resolutions = [10, 20, 60]
+    list_resolutions = [10, 20, 60]
 
-    # for zip_file in image_manager_obj.zipped_files:
-    #     for resolution in list_resolutions[:2]:
-    #         root_name_dir = image_manager_obj.extract_images_in_zip(zip_file, resolution=resolution)
-    #         ManageImageries.clip_images_in_dir(root_name_dir, "cut_poly.shp")
-    #         shutil.rmtree(root_name_dir)
+    for zip_file in image_manager_obj.zipped_files:
+        for resolution in list_resolutions[:2]:
+            root_name_dir = image_manager_obj.extract_images_in_zip(zip_file, resolution=resolution)
+            # TODO: If directory exists ignore clipping
+            ManageImageries.clip_images_in_dir(root_name_dir, resolution=resolution,
+                                               clip_geojson="vectors/cut_poly.geojson")
+            shutil.rmtree(root_name_dir)
+            logger.info("{} resolution {} done".format(zip_file, resolution))
 
-    geotiff_lst, dates, bands = image_manager_obj.create_cube_with_clipped_images_in_dir('clipped_images')
-    logger.info(sorted(bands))
-    logger.info(dates)
+    # Plot Bands with matplotlib
+    ManageImageries.plot_images(dir_name="clipped_images/20210301", resolution=20, poly_cut="vectors/cut_poly.geojson")
+
+    # # TODO: add a function to create a mosaic from all the images in the directory
+    # geotiff_lst, dates, bands = image_manager_obj.create_cube_with_clipped_images_in_dir('clipped_images')
+    # logger.info(sorted(bands))
+    # logger.info(dates)
 
 
 if __name__ == '__main__':
